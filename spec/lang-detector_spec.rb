748b6b8d384799634352cb94c54a63a77425c387
bound1ess <bound1ess.dev@gmail.com>
2015-01-15 20:54:04 +0600

    Refactor specs.

--- spec/lang-detector_spec.rb
+++ spec/lang-detector_spec.rb
@@ -1,27 +1,23 @@
 require_relative "../lib/lang-detector"
 
 RSpec.describe(Toolset::LangDetector) do
+
     before(:all) do
         @detector = Toolset::LangDetector.new
+        @projects_dir = File.dirname(__FILE__) + "/../sample-projects"
     end
 
     it("detects a PHP project") do
-        project_path = File.dirname(__FILE__) + "/../sample-projects/php"
-
-        expect(@detector.detect(project_path)).to eq(:PHP)
+        expect(@detector.detect("#{@projects_dir}/php")).to eq(:PHP)
     end
 
     it("detects a Ruby project") do
-        project_path = File.dirname(__FILE__) + "/../sample-projects/ruby"
-
-        expect(@detector.detect(project_path)).to eq(:ruby)
+        expect(@detector.detect("#{@projects_dir}/ruby")).to eq(:ruby)
     end
 
     it("detects a JS project") do
-        base_project_path = File.dirname(__FILE__) + "/../sample-projects/js"
-
-        expect(@detector.detect(base_project_path + "/bower")).to eq(:js)
-        expect(@detector.detect(base_project_path + "/npm-component")).to eq(:js)
-        expect(@detector.detect(base_project_path + "/npm-package")).to eq(:js)
+        expect(@detector.detect("#{@projects_dir}/js/bower")).to eq(:js)
+        expect(@detector.detect("#{@projects_dir}/js/npm-component")).to eq(:js)
+        expect(@detector.detect("#{@projects_dir}/js/npm-package")).to eq(:js)
     end
 end
