041c8314bea4e55e6257206784af7db0298c421d
bound1ess <bound1ess.dev@gmail.com>
2015-01-16 00:08:37 +0600

    Add CLI.

---
+++ lib/cli.rb
@@ -0,0 +1,15 @@
+require "thor"
+
+module Toolset
+    class CLI < Thor
+        desc("scan-project PATH", "Scan a project located at PATH")
+        def scan_project(path)
+            puts "Scanning a project located at #{path}..."
+        end
+
+        desc("scan-projects PATH", "Scan projects located at PATH")
+        def scan_projects(path)
+            puts "Scanning projects located at #{path}..."
+        end
+    end
+end
