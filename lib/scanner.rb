4d76d208fe4fa8cc14df5f025785345e8f0509ce
bound1ess <bound1ess.dev@gmail.com>
2015-01-17 12:42:59 +0600

    Scanner scans one project.

--- lib/scanner.rb
+++ lib/scanner.rb
@@ -1,7 +1,43 @@
 module Toolset
     class Scanner
+        def initialize
+            @managers = {
+                :PHP  => ["composer"],
+                :ruby => ["gem"],
+                :js   => ["bower", "NPM"],
+            }
+
+            @formatters = ["json", "plain"]
+
+            @factories = {
+                :manager   => PackageManagers::ManagerFactory.new,
+                :formatter => OutputFormatters::FormatterFactory.new,
+            }
+
+            @lang = LangDetector.new
+        end
+
         def scan_project(path)
-            # ...
+            if not Dir.exists?(path)
+                raise IOError
+            end
+
+            project_name = File.basename(path)
+            project_language = @lang.detect(path)
+            project_packages = []
+
+            @managers[project_language].map { |manager|
+                manager = @factories[:manager].new_manager(manager)
+
+                project_packages.concat(manager.get_dependencies(path))
+            }
+
+            {
+                project_name.to_sym => {
+                    :language => project_language.to_s.downcase,
+                    :packages => project_packages,
+                },
+            }
         end
 
         def scan_projects(path)
--- spec/scanner_spec.rb
+++ spec/scanner_spec.rb
@@ -6,7 +6,19 @@ RSpec.describe(Toolset::Scanner) do
     end
 
     it("scans a project") do
-        # TODO
+        expect(@scanner.scan_project(File.dirname(__FILE__) + "/../sample-projects/php"))
+            .to eq({
+                :php => {
+                    :language => "php",
+                    :packages => ["phpspec/phpspec", "phpunit/phpunit"],
+                },
+            })
+    end
+
+    it("raises IOError if something goes wrong") do
+        expect {
+            @scanner.scan_project("invalid-path")
+        }.to raise_error(IOError)
     end
 
     it("scans 1+ projects") do
