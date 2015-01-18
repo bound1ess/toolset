6c40bbd75c9e4d00864062d37537d7e463a1a154
bound1ess <bound1ess.dev@gmail.com>
2015-01-18 13:26:35 +0600

    Add support for not open-ended dependencies in Gemfile.

--- lib/package-managers/gem.rb
+++ lib/package-managers/gem.rb
@@ -10,7 +10,7 @@ module Toolset
 
                 gemfile = File.read(path)
 
-                gemfile.scan(/gem\s([^\s]+)/).map { |gem|
+                gemfile.scan(/gem\s([^\s,]+)/).map { |gem|
                     gem.first[1...-1]
                 }
             end
